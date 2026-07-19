c9eed44d8a8a02b5c676116e2fc31802a506ffe2
XX <freecoder.xx@gmail.com>
2026-07-19 21:41:58 +0300

    feat: add fa house icon

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
     feature = "fontawesome-solid",
 ))]
 pub mod fontawesome;
-#[cfg(any(feature = "fontawesome-ext", feature = "fontawesome-ext-regular",))]
+#[cfg(any(feature = "fontawesome-ext", feature = "fontawesome-ext-regular"))]
 pub mod fontawesome_ext;
 #[cfg(feature = "hypertext")]
 pub mod hypertext;
